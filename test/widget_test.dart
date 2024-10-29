91d7facdcbb0ef8330e5249d018355d84b46ec05
Keanna Dirga <keanna.dirga@uri.edu>
2024-10-29 08:23:46 -0400

    Update METRICS.md
    Added Metric 3: Daily Active Users

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,6 +12,6 @@ import 'package:plant_planner/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(const MyApp());
   });
 }
